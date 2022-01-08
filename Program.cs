2d643f18974a9d0d962cbb7cda942f303efcbced
Spidicy12 <94277691+Spidicy12@users.noreply.github.com>
2022-01-07 19:24:08 -0700

    Add files via upload
    this is an unfinished product but never the less this is a program that you enter the name of our party members (this would be used for airsoft or lazertag parties) and take those names and allow you to make a party and edit the members of the party.// this is a fairly simple program

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace balls
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("who is the first memeber of your party ");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("who is the second member of you party ");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("is there another member of your party [Y/N]");
+            string choice1 = Console.ReadLine();
+            if (choice1 == "y")
+            {
+                Console.WriteLine("who is the next memeber of the party ");
+                string name3 = Console.ReadLine();
+                Console.WriteLine("is there any more members of your party? ");
+                string ab = Console.ReadLine();
+                while (ab == "y")
+                {
+                    Console.WriteLine("name other member of your party");
+                    string name4 = 
+                }
+            }
+        }
+    }
+}
